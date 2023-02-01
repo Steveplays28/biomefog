796d415fe81d4e5357e2ac0f08e99ed24b6cccb5
Steveplays28 <djspaargaren@outlook.com>
2023-02-01 23:37:08 +0100

    refactor: Make `fogColor` transient

--- src/main/java/io/github/steveplays28/biomefog/config/BiomeFogConfig.java
+++ src/main/java/io/github/steveplays28/biomefog/config/BiomeFogConfig.java
@@ -5,7 +5,8 @@ import org.joml.Vector4f;
 
 public class BiomeFogConfig {
 	public Vec3d skyColor = new Vec3d(0.68f, 0.83f, 1f);
-	public Vector4f fogColor = new Vector4f(this.skyColor.toVector3f(), 1f);
+
+	public transient Vector4f fogColor = new Vector4f(this.skyColor.toVector3f(), 1f);
 	public Vector4f fogColorSwamp = new Vector4f(0.28f, 0.28f, 0.05f, 1f);
 	public Vector4f fogColorSnow = new Vector4f(0.96f, 0.98f, 0.94f, 1f);
 	public Vector4f fogColorDesert = new Vector4f(0.84f, 0.78f, 0.6f, 1f);
