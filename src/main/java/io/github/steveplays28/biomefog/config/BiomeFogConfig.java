f1fc438e162c72ca8ca118cadd2897c677ed3df6
Steveplays28 <djspaargaren@outlook.com>
2023-04-23 13:07:15 +0200

    feat: Add more biomes to the config

--- src/main/java/io/github/steveplays28/biomefog/config/BiomeFogConfig.java
+++ src/main/java/io/github/steveplays28/biomefog/config/BiomeFogConfig.java
@@ -15,8 +15,18 @@ public class BiomeFogConfig {
 
 	public Map<String, Vector4f> fogColors = Map.ofEntries(
 			entry("minecraft:swamp", new Vector4f(0.28f, 0.28f, 0.05f, 1f)),
+			entry("minecraft:mangrove_swamp", new Vector4f(0.28f, 0.28f, 0.05f, 1f)),
 			entry("minecraft:jungle", new Vector4f(0.15f, 0.42f, 0.13f, 1f)),
-			entry("minecraft:snow", new Vector4f(0.96f, 0.98f, 0.94f, 1f)),
-			entry("minecraft:desert", new Vector4f(0.84f, 0.78f, 0.6f, 1f))
+			entry("minecraft:bamboo_jungle", new Vector4f(0.15f, 0.42f, 0.13f, 1f)),
+			entry("minecraft:sparse_jungle", new Vector4f(0.15f, 0.42f, 0.13f, 1f)),
+			entry("minecraft:snowy_plains", new Vector4f(0.96f, 0.98f, 0.94f, 1f)),
+			entry("minecraft:snowy_slopes", new Vector4f(0.96f, 0.98f, 0.94f, 1f)),
+			entry("minecraft:snowy_taiga", new Vector4f(0.96f, 0.98f, 0.94f, 1f)),
+			entry("minecraft:frozen_peaks", new Vector4f(0.96f, 0.98f, 0.94f, 1f)),
+			entry("minecraft:jagged_peaks", new Vector4f(0.96f, 0.98f, 0.94f, 1f)),
+			entry("minecraft:desert", new Vector4f(0.84f, 0.78f, 0.6f, 1f)),
+			entry("minecraft:badlands", new Vector4f(0.75f, 0.4f, 0.13f, 1f)),
+			entry("minecraft:eroded_badlands", new Vector4f(0.75f, 0.4f, 0.13f, 1f)),
+			entry("minecraft:wooded_badlands", new Vector4f(0.75f, 0.4f, 0.13f, 1f))
 	);
 }
