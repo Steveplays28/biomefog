2cb61dfbb01adbd93bb97c15f4609826d864d52e
Steveplays28 <djspaargaren@outlook.com>
2023-04-24 15:26:27 +0200

    fix: Disable `DimensionEffects` mixin so sunrise/sunset fog appears normally

--- src/main/java/io/github/steveplays28/biomefog/mixin/DimensionEffectsMixin.java
+++ src/main/java/io/github/steveplays28/biomefog/mixin/DimensionEffectsMixin.java
@@ -1,19 +1,14 @@
 package io.github.steveplays28.biomefog.mixin;
 
-import io.github.steveplays28.biomefog.client.BiomeFogClient;
 import net.minecraft.client.render.DimensionEffects;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(DimensionEffects.class)
 public class DimensionEffectsMixin {
 	// Changes sunrise/sunset fog color override (fog around the sun)
-	@Inject(method = "getFogColorOverride", at = @At("RETURN"), cancellable = true)
-	public void getFogColorOverrideInject(float skyAngle, float tickDelta, CallbackInfoReturnable<float[]> cir) {
-		// TODO: Figure out if this changes anything at all, and if it does, what it changes
-		cir.setReturnValue(null);
+//	@Inject(method = "getFogColorOverride", at = @At("RETURN"), cancellable = true)
+//	public void getFogColorOverrideInject(float skyAngle, float tickDelta, CallbackInfoReturnable<float[]> cir) {
+//		cir.setReturnValue(null);
 //		cir.setReturnValue(BiomeFogClient.fogColor);
-	}
+//	}
 }
