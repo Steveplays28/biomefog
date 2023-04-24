7e862bc79c53aeded70e61ef4d08e2d3aa5567b0
Steveplays28 <djspaargaren@outlook.com>
2023-04-24 20:56:23 +0200

    refactor: Remove unused mixins
    Also improved code and mixin comments.

--- src/main/java/io/github/steveplays28/biomefog/mixin/BackgroundRendererMixin.java
+++ src/main/java/io/github/steveplays28/biomefog/mixin/BackgroundRendererMixin.java
@@ -17,7 +17,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BackgroundRenderer.class)
@@ -66,21 +65,8 @@ public abstract class BackgroundRendererMixin {
 		return viewDistance - f;
 	}
 
-	//	 Removes fog from clouds (makes clouds visible)
-//	@Inject(method = "setFogBlack", at = @At("HEAD"), cancellable = true)
-//	private static void setFogBlackInject(CallbackInfo ci) {
-//		RenderSystem.setShaderFogColor(0.67f, 0.8f, 0.95f, 1f);
-//		ci.cancel();
-//	}
-
 	@Shadow
 	public static void clearFog() {
-	}
-
-	@ModifyVariable(method = "render", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/client/world/ClientWorld;getRainGradient(F)F"), index = 1, argsOnly = true)
-	private static float renderModifyF(float value) {
-		BiomeFogClient.LOGGER.info("original value: {} ----- new value: -1f", value);
-		return -1f;
 	}
 
 	@Inject(method = "render", at = @At(value = "HEAD"), cancellable = true)
--- src/main/java/io/github/steveplays28/biomefog/mixin/WorldRendererMixin.java
+++ src/main/java/io/github/steveplays28/biomefog/mixin/WorldRendererMixin.java
@@ -1,8 +1,6 @@
 package io.github.steveplays28.biomefog.mixin;
 
-import io.github.steveplays28.biomefog.config.BiomeFogConfigLoader;
 import net.minecraft.client.render.WorldRenderer;
-import net.minecraft.util.math.Vec3d;
 import org.joml.Math;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,18 +8,17 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(WorldRenderer.class)
 public class WorldRendererMixin {
-	// The lower the multiplier is the more sky that appears to render with my custom color (from the renderSky @ModifyVariable above) at the top of the skybox :o
+	// The amount of transition between the top of the sky and the bottom of the sky
 	@ModifyVariable(method = "renderSky(Lnet/minecraft/client/render/BufferBuilder;F)Lnet/minecraft/client/render/BufferBuilder$BuiltBuffer;", at = @At(value = "INVOKE_ASSIGN", target = "Ljava/lang/Math;signum(F)F"), argsOnly = true)
 	private static float renderSkyBufferBuilderInject(float g) {
-		return Math.lerp(Math.signum(16.0f) * 16.0f, Math.signum(16.0f) * 8.0f, BiomeFogConfigLoader.CONFIG.fogColorLerpTime);
+		return Math.signum(16.0f) * 8.0f;
 	}
 
-	// Changes sky color in a circle gradient around the top of the skybox
-	// The gradient appears to always want to go towards gray/blueish at the horizon in vanilla (fixed by renderSkyBufferBuilderInject below)
-	@ModifyVariable(method = "renderSky*", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/client/world/ClientWorld;getSkyColor(Lnet/minecraft/util/math/Vec3d;F)Lnet/minecraft/util/math/Vec3d;"))
-	public Vec3d renderSkyInject(Vec3d original) {
-		return BiomeFogConfigLoader.CONFIG.skyColor;
-	}
+	// Changes the color of the top of the sky
+//	@ModifyVariable(method = "renderSky*", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/client/world/ClientWorld;getSkyColor(Lnet/minecraft/util/math/Vec3d;F)Lnet/minecraft/util/math/Vec3d;"))
+//	public Vec3d renderSkyInject(Vec3d original) {
+//		return BiomeFogConfigLoader.CONFIG.skyColor;
+//	}
 
 	// Changes clouds color
 	// Already fixed by Sodium, only required on vanilla
