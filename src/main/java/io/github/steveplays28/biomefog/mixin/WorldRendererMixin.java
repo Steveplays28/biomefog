2007cda42f61b30688d5dcf0fa9027c39b64cf39
Steveplays28 <djspaargaren@outlook.com>
2023-04-27 19:07:31 +0200

    fix: Fix crash due to `@ModifyVariable` mixin
    The mixin wasn't specific enough, causing it to fail in non-dev environments.

--- src/main/java/io/github/steveplays28/biomefog/mixin/WorldRendererMixin.java
+++ src/main/java/io/github/steveplays28/biomefog/mixin/WorldRendererMixin.java
@@ -20,7 +20,8 @@ public class WorldRendererMixin {
 	}
 
 	// Changes the color of the top of the sky
-	@ModifyVariable(method = "renderSky*", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/client/world/ClientWorld;getSkyColor(Lnet/minecraft/util/math/Vec3d;F)Lnet/minecraft/util/math/Vec3d;"))
+	@ModifyVariable(method = "renderSky(Lnet/minecraft/client/util/math/MatrixStack;Lorg/joml/Matrix4f;FLnet/minecraft/client/render/Camera;ZLjava/lang/Runnable;)V", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/client/world/ClientWorld;getSkyColor(Lnet/minecraft/util/math/Vec3d;F)Lnet/minecraft/util/math/Vec3d;"))
+	@SuppressWarnings("InvalidInjectorMethodSignature")
 	public Vec3d renderSkyInject(Vec3d original) {
 		var world = MinecraftClient.getInstance().world;
 		if (world == null) return original;
@@ -34,11 +35,4 @@ public class WorldRendererMixin {
 
 		return original.add(BiomeFogConfigLoader.CONFIG.skyColorAddition);
 	}
-
-	// Changes clouds color
-	// Already fixed by Sodium, only required on vanilla
-//	@ModifyVariable(method = "renderClouds*", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/client/world/ClientWorld;getCloudsColor(F)Lnet/minecraft/util/math/Vec3d;"))
-//	public Vec3d renderClouds(Vec3d original) {
-//		return new Vec3d(1f, 1f, 1f);
-//	}
 }
