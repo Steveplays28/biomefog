8537e9fa182c8e85f986e440dfb9d5f3b332f59d
Steveplays28 <djspaargaren@outlook.com>
2023-07-15 18:08:41 +0200

    refactor: Add folder support to config system

---
+++ src/main/java/io/github/steveplays28/biomefog/config/user/BiomeFogBlackListedWorlds.java
@@ -0,0 +1,10 @@
+package io.github.steveplays28.biomefog.config.user;
+
+import io.github.steveplays28.biomefog.config.BiomeFogBaseConfig;
+
+import java.util.Collections;
+import java.util.List;
+
+public class BiomeFogBlackListedWorlds implements BiomeFogBaseConfig {
+	public List<String> blackListedWorlds = Collections.emptyList();
+}
--- src/main/java/io/github/steveplays28/biomefog/mixin/BackgroundRendererMixin.java
+++ src/main/java/io/github/steveplays28/biomefog/mixin/BackgroundRendererMixin.java
@@ -2,7 +2,6 @@ package io.github.steveplays28.biomefog.mixin;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import io.github.steveplays28.biomefog.client.BiomeFogClient;
-import io.github.steveplays28.biomefog.config.BiomeFogConfigLoader;
 import io.github.steveplays28.biomefog.util.MathUtil;
 import io.github.steveplays28.biomefog.util.RenderSystemUtil;
 import io.github.steveplays28.biomefog.util.TimeUtil;
@@ -18,15 +17,19 @@ import net.minecraft.registry.tag.BiomeTags;
 import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Map;
 
+import static io.github.steveplays28.biomefog.config.BiomeFogConfigLoader.BiomeFogConfigurations.CONFIG;
+
 @Environment(EnvType.CLIENT)
 @Mixin(BackgroundRenderer.class)
 public abstract class BackgroundRendererMixin {
+	@Unique
 	private static Boolean shouldApplyFog = true;
 
 	@Inject(method = "applyFog", at = @At("TAIL"))
@@ -46,8 +49,8 @@ public abstract class BackgroundRendererMixin {
 		BiomeFogClient.currentDimension = WorldUtil.GetDimension().toString();
 
 		// Check if current dimension is enabled
-		if (!BiomeFogConfigLoader.CONFIG.enabledDimensions.contains(
-				BiomeFogClient.currentDimension) && !BiomeFogConfigLoader.CONFIG.enabledDimensions.contains("all")) {
+		if (!CONFIG.enabledDimensions.contains(BiomeFogClient.currentDimension) && !CONFIG.enabledDimensions.contains(
+				"all")) {
 			return;
 		}
 
@@ -61,29 +64,29 @@ public abstract class BackgroundRendererMixin {
 		Vec3d defaultFogColor;
 
 		if (world.isRaining() || world.isThundering()) {
-			fogStartAdditions = BiomeFogConfigLoader.CONFIG.fogStartAdditionsRain;
-			fogEndAdditions = BiomeFogConfigLoader.CONFIG.fogEndAdditionsRain;
-			fogColors = BiomeFogConfigLoader.CONFIG.fogColorsRain;
-			defaultFogColor = BiomeFogConfigLoader.CONFIG.defaultFogColorRain;
+			fogStartAdditions = CONFIG.fogStartAdditionsRain;
+			fogEndAdditions = CONFIG.fogEndAdditionsRain;
+			fogColors = CONFIG.fogColorsRain;
+			defaultFogColor = CONFIG.defaultFogColorRain;
 		} else if (TimeUtil.isNight(world)) {
-			fogStartAdditions = BiomeFogConfigLoader.CONFIG.fogStartAdditionsNight;
-			fogEndAdditions = BiomeFogConfigLoader.CONFIG.fogEndAdditionsNight;
-			fogColors = BiomeFogConfigLoader.CONFIG.fogColorsNight;
-			defaultFogColor = BiomeFogConfigLoader.CONFIG.defaultFogColorNight;
+			fogStartAdditions = CONFIG.fogStartAdditionsNight;
+			fogEndAdditions = CONFIG.fogEndAdditionsNight;
+			fogColors = CONFIG.fogColorsNight;
+			defaultFogColor = CONFIG.defaultFogColorNight;
 		} else {
-			fogStartAdditions = BiomeFogConfigLoader.CONFIG.fogStartAdditions;
-			fogEndAdditions = BiomeFogConfigLoader.CONFIG.fogEndAdditions;
-			fogColors = BiomeFogConfigLoader.CONFIG.fogColors;
-			defaultFogColor = BiomeFogConfigLoader.CONFIG.defaultFogColor;
+			fogStartAdditions = CONFIG.fogStartAdditions;
+			fogEndAdditions = CONFIG.fogEndAdditions;
+			fogColors = CONFIG.fogColors;
+			defaultFogColor = CONFIG.defaultFogColor;
 		}
 
-		BiomeFogConfigLoader.CONFIG.fogStartAddition = MathUtil.lerp(BiomeFogConfigLoader.CONFIG.fogStartAddition,
+		CONFIG.fogStartAddition = MathUtil.lerp(CONFIG.fogStartAddition,
 				fogStartAdditions.getOrDefault(
 						BiomeFogClient.currentBiome,
 						fogStartAdditions.getOrDefault(BiomeFogClient.currentDimension, 0f)
 				), 0.001f
 		);
-		BiomeFogConfigLoader.CONFIG.fogEndAddition = MathUtil.lerp(BiomeFogConfigLoader.CONFIG.fogEndAddition,
+		CONFIG.fogEndAddition = MathUtil.lerp(CONFIG.fogEndAddition,
 				fogEndAdditions.getOrDefault(
 						BiomeFogClient.currentBiome,
 						fogEndAdditions.getOrDefault(BiomeFogClient.currentDimension, 0f)
@@ -92,7 +95,7 @@ public abstract class BackgroundRendererMixin {
 
 		if (fogColors.containsKey(BiomeFogClient.currentBiome)) {
 			currentBiomeFogColor = fogColors.get(BiomeFogClient.currentBiome);
-		} else if (BiomeFogConfigLoader.CONFIG.fogColorsRain.containsKey(BiomeFogClient.currentDimension)) {
+		} else if (CONFIG.fogColorsRain.containsKey(BiomeFogClient.currentDimension)) {
 			currentBiomeFogColor = fogColors.get(BiomeFogClient.currentDimension);
 		} else {
 			currentBiomeFogColor = defaultFogColor;
@@ -103,15 +106,13 @@ public abstract class BackgroundRendererMixin {
 				BiomeFogClient.currentBiome) || fogStartAdditions.containsKey(
 				BiomeFogClient.currentDimension) || fogEndAdditions.containsKey(
 				BiomeFogClient.currentDimension) || fogColors.containsKey(BiomeFogClient.currentDimension)) {
-			BiomeFogConfigLoader.CONFIG.fogColorLerpTime = MathUtil.clamp(
-					0f, 1f, BiomeFogConfigLoader.CONFIG.fogColorLerpTime + tickDelta * 0.001f);
+			CONFIG.fogColorLerpTime = MathUtil.clamp(0f, 1f, CONFIG.fogColorLerpTime + tickDelta * 0.001f);
 		} else {
-			BiomeFogConfigLoader.CONFIG.fogColorLerpTime = MathUtil.clamp(
-					0f, 1f, BiomeFogConfigLoader.CONFIG.fogColorLerpTime - tickDelta * 0.001f);
+			CONFIG.fogColorLerpTime = MathUtil.clamp(0f, 1f, CONFIG.fogColorLerpTime - tickDelta * 0.001f);
 		}
 
 		// Linearly interpolate fog color
-		BiomeFogConfigLoader.CONFIG.fogColor = BiomeFogConfigLoader.CONFIG.fogColor.lerp(currentBiomeFogColor, 0.001f);
+		CONFIG.fogColor = CONFIG.fogColor.lerp(currentBiomeFogColor, 0.001f);
 
 		// Update fog
 		if (cameraSubmersionType.equals(CameraSubmersionType.WATER)) {
@@ -130,15 +131,12 @@ public abstract class BackgroundRendererMixin {
 
 			RenderSystem.setShaderFogEnd(fogEnd);
 		} else {
-			RenderSystem.setShaderFogStart(
-					MathUtil.lerp(vanillaFogStart(viewDistance), 0f + BiomeFogConfigLoader.CONFIG.fogStartAddition,
-							BiomeFogConfigLoader.CONFIG.fogColorLerpTime
-					));
+			RenderSystem.setShaderFogStart(MathUtil.lerp(vanillaFogStart(viewDistance), 0f + CONFIG.fogStartAddition,
+					CONFIG.fogColorLerpTime
+			));
 			RenderSystem.setShaderFogEnd(
-					MathUtil.lerp(viewDistance, viewDistance / 3 + BiomeFogConfigLoader.CONFIG.fogEndAddition,
-							BiomeFogConfigLoader.CONFIG.fogColorLerpTime
-					));
-			RenderSystemUtil.setShaderFogColor(BiomeFogConfigLoader.CONFIG.fogColor);
+					MathUtil.lerp(viewDistance, viewDistance / 3 + CONFIG.fogEndAddition, CONFIG.fogColorLerpTime));
+			RenderSystemUtil.setShaderFogColor(CONFIG.fogColor);
 		}
 
 		// Re-render light and dark skies to update WorldRendererMixin changes
@@ -146,10 +144,11 @@ public abstract class BackgroundRendererMixin {
 		MinecraftClient.getInstance().worldRenderer.renderDarkSky();
 
 		// DEBUG
-//		BiomeFogClient.LOGGER.info("\nfogColor: {}\nactualFogColor: {}\nlerpTime: {}", BiomeFogConfigLoader.CONFIG.fogColor, RenderSystem.getShaderFogColor(), BiomeFogConfigLoader.CONFIG.fogColorLerpTime);
-//		BiomeFogClient.LOGGER.info("fogStartAddition: {}\nfogEndAddition: {}", BiomeFogConfigLoader.CONFIG.fogStartAddition, BiomeFogConfigLoader.CONFIG.fogEndAddition);
+//		BiomeFogClient.LOGGER.info("\nfogColor: {}\nactualFogColor: {}\nlerpTime: {}", CONFIG.fogColor, RenderSystem.getShaderFogColor(), CONFIG.fogColorLerpTime);
+//		BiomeFogClient.LOGGER.info("fogStartAddition: {}\nfogEndAddition: {}", CONFIG.fogStartAddition, CONFIG.fogEndAddition);
 	}
 
+	@Unique
 	private static float vanillaFogStart(float viewDistance) {
 		float f = MathUtil.clamp(viewDistance / 10.0f, 4.0f, 64.0f);
 		return viewDistance - f;
@@ -163,7 +162,7 @@ public abstract class BackgroundRendererMixin {
 	@Inject(method = "render", at = @At(value = "HEAD"), cancellable = true)
 	private static void renderInject(Camera camera, float tickDelta, ClientWorld world, int viewDistance, float skyDarkness, CallbackInfo ci) {
 		if (shouldApplyFog) {
-			RenderSystemUtil.setClearColor(BiomeFogConfigLoader.CONFIG.fogColor);
+			RenderSystemUtil.setClearColor(CONFIG.fogColor);
 			clearFog();
 			ci.cancel();
 		}
@@ -173,7 +172,7 @@ public abstract class BackgroundRendererMixin {
 	@Inject(method = "setFogBlack", at = @At("HEAD"), cancellable = true)
 	private static void setFogBlackInject(CallbackInfo ci) {
 		if (shouldApplyFog) {
-			RenderSystemUtil.setShaderFogColor(BiomeFogConfigLoader.CONFIG.fogColor);
+			RenderSystemUtil.setShaderFogColor(CONFIG.fogColor);
 			ci.cancel();
 		}
 	}
