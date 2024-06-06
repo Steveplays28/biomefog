df232abb6e4a2af6cfb84cb1cd097c8e4c843242
ipz233_vvv <jumpergooog@gmail.com>
2024-06-06 10:01:41 +0300

    night check fix

--- src/main/java/io/github/steveplays28/biomefog/util/TimeUtil.java
+++ src/main/java/io/github/steveplays28/biomefog/util/TimeUtil.java
@@ -4,6 +4,6 @@ import net.minecraft.client.world.ClientWorld;
 
 public class TimeUtil {
 	public static boolean isNight(ClientWorld world) {
-		return world.getTimeOfDay() >= 13000 && world.getTimeOfDay() <= 23000;
+		return world.getTimeOfDay()%24000 >= 13000 && world.getTimeOfDay()%24000 <= 23000;
 	}
 }
